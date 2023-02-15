591e2442b1ce2ddf5d5addd7303d6fd4e48b44df
PauloK3tchup <passandoalimpo13@gmail.com>
2023-02-15 11:00:51 -0300

    Mr

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Mr.</Text>
+      <Image source={require('./assets/MR.jpg')} />
+      <Text>Beast</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +16,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FF06F4',
     alignItems: 'center',
     justifyContent: 'center',
   },
